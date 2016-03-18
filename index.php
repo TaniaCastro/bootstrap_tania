3626e8982845c1a87fd70aedb1346df8d7b6aee6
TaniaCastro <tani.cas2011@gmail.com>
2016-03-18 13:53:27 -0500

    iniciando proyecto

---
+++ index.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+	<link rel="stylesheet" href="public/css/estilos.css">
+</head>
+<body>
+<style>
+		body{
+			background-image: url(public/img/4.jpg);
+		}
+
+		h1{
+			@import url(https://fonts.googleapis.com/css?family=Lobster);
+
+	margin: 0;
+	font-size: 5em;
+	text-align: center;
+	color: transparent;
+	font-weight: normal;
+	background: linear-gradient(
+		pink,pink, blue,red, pink);
+	-webkit-background-clip: text;
+	position:relative;
+	&: after{
+		content:"texto con fondo degradado";
+		position:absolute;
+		top: 3px;
+		left: 3px;
+		color: #000;
+		z-index: -1;
+
+border-bottom: dotted 2px #ccc ;
+text-align:center; 
+}
+		}
+		a{
+			background-color:lightblue;
+			
+		}
+		a:hover{}
+		#main_contair{}
+		#content{
+			background-color:red;
+			color:red;
+		}
+		#descripcion{
+			background-color:red;
+			color:red;
+		}
+		#bonton-content{
+			color:red;
+			
+		}
+	</style>
+<body>
+<header>
+			<div id="bonton-content">
+			 <h1><center>Mi Favorito... =D</center></h1></div>
+			 <br>
+        </header>
+
+
+<div class="container">
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">TIENDA VIRTUAL</a>
+			</div>
+	
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Link</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li><a href="#">Separated link</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+</div>
+
+<h1 class="TANIA">MOMENTOS BONITOS CONTIGO :)</h1></div></nav></div>
+<div class="container">
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+<p>Recordaba nuestras locuras , nuestras peleas sin sentido, cuando reniegas, cuando sonries, cuando 
+              empiesas a ingreirme, cuando me abrasas y uufffff muchas cosas mas, ya sean buenas o malaas. los 
+              recuerdos k bienen a mi mente no terminan, por que son demaciadas, ah y no me preguntes que si 
+              talves no te quiero o no te ame, nisiquiera lo digas, por que sabes de sobra que cada dia te buelbo
+               a elegir y te vuelvo a elegir, no digas que talves solo me tragiste problemas , no digas eso por 
+               que me parte el alma,sabes k eres lo mejor de mi </p></a>
+			</div>
+</div></nav></div>
+
+
+	<div class="container">
+		<div class="row">
+
+		<div class="col-md-4"> <img src="public/img/11.jpg" class="img-circle">
+             <img src="public/img/20.jpg" class="img-circle">
+
+		</div>
+		<div class="col-md-4"> <img src="public/img/12.jpg" class="img-circle">
+             <img src="public/img/21.jpg" class="img-circle">
+
+		</div>
+		<div class="col-md-4"> <img src="public/img/15.jpg" class="img-circle">
+             <img src="public/img/22.jpg" class="img-circle">
+
+		</div>
+		
+
+		</style>
+<body>
+<header>
+			<div id="bonton-content">
+			 <h1><center>YO AMO LA MUSICA ... :) :D </center></h1></div>
+			 <br>
+        </header>
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<!-- Marca y de palanca quedan agrupados para una mejor visualización en dispositivos móviles -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<ul class="nav navbar-nav">
+			<li class="navbar-brand" href="#">Inicio</a></li>
+		</div>
+
+		<!-- Recoger el enlaces de navegación , formularios y otros contenidos para alternar -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li class="#"><a href="VideosTJW_app/index2.html">Videos</a></li>
+				<li><a href="index3.html">Imagnes</a></li>
+			</ul>
+			<form class="navbar-form navbar-left" role="buscar">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="escribir">
+				</div>
+				<button type="buscador" class="btn btn-default">buscador</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="http://taniacas2011.wix.com/twaniedtjw">mas videos</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Mas<b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Inicio</a></li>
+						<li><a href="VideosTJW_app/index2.html">Videos</a></li>
+						<li><a href="http://taniacas2011.wix.com/twaniedtjw">Mas informacion</a></li>
+						<li><a href="http://cristiantjw3.wix.com/twanied">Mas videos</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div>
+</nav>
+
+<div id="carousel-id" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="item">
+			<header>
+            <h1><center><img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="public/img/3.jpg" height="450" width="1000"></center></h1>
+        </header>
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Tokio Hotel</h1>
+					<p>
+						"Estoy gritando en la cima del mundo!! no me sientes??"</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Videos</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item">
+		<header>
+            <h1><center><img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="public/img/1.jpg" height="450" width="1000"></center></h1>
+        </header>
+
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Tokio Hotel</h1>
+					<p>Vamos! Hay dias Cuando te sientes tan pequeño Y sabes 
+						Que deberias estar tan alto Piensas que no escogistes. Mira la tierra Mira que hacemos Aqui y ahora Te necesitamos El silencio puede destruir Levantate y alza tu voz</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Videos</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item active"
+		<header>
+            <h1><center><img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="public/img/2.jpg" height="450" width="1000"></center></h1>
+        </header>
+					
+		
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Tokio Hotel</h1>
+					<p>Yo estaré ahí para abrazarte muy cerca de mi 
+						Cuando estés fuera y no puedas entrar 
+						Te enseñaré, eres mejor de lo que sabes 
+						Cuando te pierdas, cuando estés solo y no puedas regresar 
+						Te encontraré cariño, te llevaré de vuelta a casa </p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Videos</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+
+
+<script src="public/js/jquery-1.12.1.js"></script>
+<script src="public/js/bootstrap.js"></script>
+
+        </div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="row">
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls>
+					<source src="public/music/Tokio Hotel - Love Who Loves You Back.mp4">
+				</video>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls>
+					<source src="public/music/Bea Miller - Young Blood (Official Video).mp4">
+				</video>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls>
+					<source src="public/music/Austin Mahone - What About Love.mp4">
+				</video>
+			</div>
+		</div>
+		 
+	</div>
+
+        <audio autoplay="true" autobuffer autoloop loop controls>
+        	<source src="public/videos/Tokio Hotel - Love Who Loves You Back.mp4">
+        	  </audio>
+	<div>
+
+
+
+	
+
+
+
+</body>
+</html
